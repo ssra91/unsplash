dc7268e0c9450961d788f6674def227fc16f86bc
sorakim <sora6418@gmail.com>
2023-11-17 21:50:47 +0900

    feat: infiniteScroll 무한스크롤 기능 구현 (InfiniteObserver)

---
+++ src/views/Home/components/MainPhotos/MainInfiniteScroll.tsx
@@ -0,0 +1,53 @@
+import styled from "@emotion/styled";
+import { useEffect, useRef } from "react";
+
+interface Props {
+  children: React.ReactNode;
+  length: number;
+  onNext: () => void;
+}
+const MainInfiniteScroll = ({ children, length, onNext }: Props) => {
+  const detectorRef = useRef<HTMLDivElement | null>(null);
+
+  const options: IntersectionObserverInit = {
+    root: null, // root 는 화면
+    rootMargin: "0px",
+    threshold: 0,
+  };
+
+  useEffect(() => {
+    // early return
+    if (detectorRef.current === null) return;
+    const callback: IntersectionObserverCallback = (nodes, observer) => {
+      nodes.forEach((entry) => {
+        if (entry.isIntersecting && length > 0) {
+          onNext();
+        }
+      });
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(detectorRef.current);
+
+    return () => {
+      if (detectorRef.current === null) return;
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      observer.unobserve(detectorRef.current);
+    };
+  }, [onNext]);
+
+  return (
+    <Container>
+      {children}
+      <Detector ref={detectorRef} />
+    </Container>
+  );
+};
+
+const Container = styled.div``;
+
+const Detector = styled.div`
+  height: 50px;
+  background-color: #18f;
+`;
+
+export default MainInfiniteScroll;
